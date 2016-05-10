24820de53e69ec599472d030fa4937a4d9512f1d
jeremie dentan <jeredentan@gmail.com>
2016-05-10 09:47:55 +0200

    commit2

---
+++ ProjetHPP_VersionMonothread-1/src/main/main.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws IOException{
+
+		Ordonnanceur ordo = new Ordonnanceur();
+		ordo.traitement();
+
+
+			//System.out.println(ordo.posts);
+			System.out.println(ordo.comments);
+	}
+
+
+
+
+
+
+
+}
